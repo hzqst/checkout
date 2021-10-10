8caa8afdb71f53adde873d5c37a2fddf70b46435
hzqst <113660872@qq.com>
2021-10-10 20:18:55 +0800

    protocol version = 0

--- src/git-command-manager.ts
+++ src/git-command-manager.ts
@@ -169,7 +169,7 @@ class GitCommandManager {
   }
 
   async fetch(refSpec: string[], fetchDepth?: number): Promise<void> {
-    const args = ['-c', 'protocol.version=2', 'fetch']
+    const args = ['-c', 'protocol.version=0', 'fetch']
     if (!refSpec.some(x => x === refHelper.tagsRefSpec)) {
       args.push('--no-tags')
     }
